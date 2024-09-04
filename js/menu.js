45c4a39945c7675b0fe2170bc90e121f60050f51
miguelromeroromero <angelromerouchiha06@gmail.com>
2024-09-04 15:00:08 -0500

    Add files via upload
    Signed-off-by: miguelromeroromero <angelromerouchiha06@gmail.com>

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+$(function(){
+var header = document.getElementById('header');
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(header);
+// initialise
+headroom.init();});
